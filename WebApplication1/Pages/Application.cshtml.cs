c1448f17799b4ad3cb84234f7157f55fb64ed244
student <student@DESKTOP-43L7716>
2022-05-30 13:20:08 +0200

    Add project files.

---
+++ WebApplication1/Pages/Application.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class ApplicationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
